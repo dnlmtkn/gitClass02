48287a0ff1feb8dd3d77d29672fa73952bf5346f
dnlmtkn <mybox.md@gmail.com>
2024-08-15 16:36:32 -0400

    third commit

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,9 +1,8 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("This is the project version #2");
-        System.out.println("This is the project version #2");
-        System.out.println("This is the project version #2");
+        System.out.println("This is the project version #3");
+
     }
 }
-        // Done with the version #2
+        // Done with the version #3
