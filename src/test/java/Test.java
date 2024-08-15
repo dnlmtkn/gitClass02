b2fabdb1da5a519a099a5d141ef946c6d732340d
dnlmtkn <mybox.md@gmail.com>
2024-08-15 16:32:53 -0400

    second commit

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,7 +1,9 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("This is the project version #1");
+        System.out.println("This is the project version #2");
+        System.out.println("This is the project version #2");
+        System.out.println("This is the project version #2");
     }
 }
-        // Done with the version #1
+        // Done with the version #2
