1bb55228bd9e6dbef996c4e28317059cfbbbf75c
dnlmtkn <mybox.md@gmail.com>
2024-08-15 17:21:30 -0400

    version 4

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,8 +1,7 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("This is the project version #3");
+        System.out.println("Version #4");
 
     }
 }
-        // Done with the version #3
+        // Done with the version #4
