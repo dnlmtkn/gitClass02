683005148d779127caf10c21ec008dbb159bc8a2
dnlmtkn <mybox.md@gmail.com>
2024-08-15 15:56:30 -0400

    first commit

---
+++ src/test/java/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("This is the project version #1");
+    }
+}
+        // Done with the version #1
